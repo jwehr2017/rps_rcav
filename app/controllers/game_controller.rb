c61b54a02c9e89a952103ca2d65eb22c1f76ffc4
jwehr2017 <joewehr@Joes-MacBook.local>
2016-10-24 20:47:58 -0500

    rps rcav hw

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,19 @@
 class GameController < ApplicationController
   def user_plays_rock
-    moves= ["Rock!","Paper!","Scissors!"]
+    moves= ["Rock","Paper","Scissors"]
     @computer_move = moves.sample
     render ("game/play_rock.html.erb")
+  end
+
+  def user_plays_paper
+    moves= ["Rock","Paper","Scissors"]
+    @computer_move = moves.sample
+    render ("game/play_paper.html.erb")
+  end
+
+  def user_plays_scissors
+    moves= ["Rock","Paper","Scissors"]
+    @computer_move = moves.sample
+    render ("game/play_scissors.html.erb")
   end
 end
